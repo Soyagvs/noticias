5b2e3aef8b0259e76e95cf0bd5888aa30df5e20a
soyagvs <agustinmartinezyt@gmail.com>
2025-02-27 16:39:06 -0300

    add comprobaciones de noticias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 export default {
     content: ["./src/**/*.{astro,js,ts,jsx,tsx,md}"], // Asegura que detecte .md
     theme: {
-      extend: {},
+      extend: {
+        screens:{
+          xs :'300px'
+        }
+      },
     },
     plugins: [require("@tailwindcss/typography")], // Activa `prose`
   };
